1722c80a18305849c896d957e7386315544228cf
Abdul Ghaffar <agscontact777@gmail.com>
2026-01-20 12:26:58 +0500

    Done with simple prints

--- basics/variables-control-structures.go
+++ basics/variables-control-structures.go
@@ -3,5 +3,31 @@ package main
 import "fmt"
 
 func main() {
+	name := "Abdul Ghaffar"
+	age := 22
+
+	// Print with new line
 	fmt.Println("Working just fine")
+
+	// Print with format specifiers
+	fmt.Print("Hello world\n");
+
+	// Pass arguments in the print
+	fmt.Print("Hello", "World", 32, 45, "\n");  // adds space automatically if arguments are numbers.
+	fmt.Print(32, 34, 534, 54, "\n");
+	fmt.Print("Check", "The", "Space\n")
+	fmt.Print(name, age, "\n")
+	n, _ := fmt.Print(name, age, "\n")  // returns number of bytes/characters
+	fmt.Print("Printed number of bytes: ", n, "\n")
+	
+	type Person struct{
+		name string
+		age int
+	};
+	p := Person{name, age}
+	// Can print any type String, int, array, slice, struct
+	fmt.Print("\nString", 22, [2]int{2, 3}, true, 3.2783, p, "\n")
+
+	// fomatting verbs
+	fmt.Printf("My name is %s, and I am %d years old.\n", name, age)
 }
