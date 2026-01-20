66d1e2b2435456d2646282f619a6b90f4d1fa409
Abdul Ghaffar <agscontact777@gmail.com>
2026-01-20 15:27:52 +0500

    Done with conditional control structures

--- basics/variables-control-structures.go
+++ basics/variables-control-structures.go
@@ -2,6 +2,98 @@ package main
 
 import "fmt"
 
+func Conditionals() {
+	fmt.Println("-----------Conditionals start-----------")
+	/*
+		Conditional control structures:
+		Syntax:
+
+		if condition {
+			Code
+		}
+
+		Parenthesis () are not allowed
+	*/
+
+	age := 10
+
+	if age < 20 {
+		fmt.Println("you are a teenager")
+	} else {
+		fmt.Println("You are not a teenager anymore.")
+	}
+
+	score := 79
+
+	if score > 90 {
+		fmt.Println("A")
+	} else if score > 80 {
+		fmt.Println("B")
+	} else {
+		fmt.Println("C")
+	}
+
+	// Go allows a short statement before condition
+	if x := 50; x > 5 { // Scope of x is in the if block
+		fmt.Println("X is greater than 5")
+	}
+
+	// ----------- Switch Statement -----------
+	// Simple switch
+	day := 5
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Frdiay")
+	case 6:
+		fmt.Println("Saturday")
+	case 7:
+		fmt.Println("Sunday")
+
+	}
+
+	// Switch with multiple conditions in one case
+	switch day {
+	case 1, 2, 3, 4, 5:
+		fmt.Println("Weekday")
+	case 6, 7:
+		fmt.Println("Weekend")
+	}
+
+	// Switch with initialization
+	switch x := 50; x {
+	case 50:
+		fmt.Println("X is 50")
+	}
+
+	// Switch without expression
+	switch {
+	case score >= 90:
+		fmt.Println("A")
+	case score >= 80:
+		fmt.Println("B")
+	case score < 80:
+		fmt.Println("C")
+	}
+	
+	// fallthrough executes the next case without satisfying the condition 
+	switch x := 1; x {
+	case 1:
+		fmt.Println("One")
+		fallthrough
+	case 2:
+		fmt.Println("Two")
+	}
+
+}
+
 func main() {
 	name := "Abdul Ghaffar"
 	age := 22.4
@@ -10,20 +102,20 @@ func main() {
 	fmt.Println("Working just fine")
 
 	// Print with format specifiers
-	fmt.Print("Hello world\n");
+	fmt.Print("Hello world\n")
 
 	// Pass arguments in the print
-	fmt.Print("Hello", "World", 32, 45, "\n");  // adds space automatically if arguments are numbers.
-	fmt.Print(32, 34, 534, 54, "\n");
+	fmt.Print("Hello", "World", 32, 45, "\n") // adds space automatically if arguments are numbers.
+	fmt.Print(32, 34, 534, 54, "\n")
 	fmt.Print("Check", "The", "Space\n")
 	fmt.Print(name, age, "\n")
-	n, _ := fmt.Print(name, age, "\n")  // returns number of bytes/characters
+	n, _ := fmt.Print(name, age, "\n") // returns number of bytes/characters
 	fmt.Print("Printed number of bytes: ", n, "\n")
-	
-	type Person struct{
+
+	type Person struct {
 		name string
-		age float64
-	};
+		age  float64
+	}
 	p := Person{name, age}
 	// Can print any type String, int, array, slice, struct
 	fmt.Print("\nString", 22, [2]int{2, 3}, true, 3.2783, p, "\n")
@@ -51,7 +143,6 @@ func main() {
 	fmt.Printf("Temp with 2 decimal places and 8 width %08.2f \n", temp)
 	fmt.Printf("Temp with 2 decimal places and 8 width %-8.2fcheck\n", temp)
 
-	
 	// Use of arguments postion
 	fmt.Printf("%[2]s is %.1[1]f years old\n", age, name)
 
@@ -66,4 +157,7 @@ func main() {
 		and when we need to concatenate different types better to use Sprintf because in concatenation
 		we will have to write a lot of extra code.
 	*/
+
+	Conditionals()
+
 }
