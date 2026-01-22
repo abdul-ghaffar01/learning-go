f525957c085a2568464c6c0678d26c628d7fdb02
Abdul Ghaffar <agscontact777@gmail.com>
2026-01-22 10:33:13 +0500

    Done with goroutines

---
+++ concurrency/goroutines.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func printNumber(num int){
+	fmt.Println(num)
+}
+
+func main() {
+	fmt.Println("-------Goroutines-------")
+
+	go printNumber(3)
+	go printNumber(4)
+	go printNumber(5)
+
+	time.Sleep(time.Second * 2)
+
+	fmt.Println("Main end")
+}
