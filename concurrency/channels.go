97affc1b0b821eb4098bd7a43322ebe1bbf546d4
Abdul Ghaffar <agscontact777@gmail.com>
2026-01-22 16:34:19 +0500

    Done with channels

--- concurrency/channels.go
+++ concurrency/channels.go
@@ -30,30 +30,30 @@ func Task1() {
 
 // Task 2  ------------------------------------------
 
-func convertToChannel(nums []int) <- chan int{
+func convertToChannel(nums []int) <-chan int {
 	out := make(chan int)
 
-	go func(){
-		for _, value := range nums{
+	go func() {
+		for _, value := range nums {
 			fmt.Println("In ctc for value ", value)
 			out <- value
 		}
 		close(out)
 	}()
-	return out;
+	return out
 }
 
-func makeSquares(in <- chan int) <- chan int{
+func makeSquares(in <-chan int) <-chan int {
 	out := make(chan int)
 
-	go func(){
-		for value := range in{
+	go func() {
+		for value := range in {
 			fmt.Println("In ms for value ", value)
 			out <- value * value
 		}
 		close(out)
 	}()
-	return out;
+	return out
 }
 
 func Task2() {
@@ -61,11 +61,34 @@ func Task2() {
 
 	firstStage := convertToChannel(nums)
 	secondStage := makeSquares(firstStage)
-	
 
 	// third stage
-	for value := range secondStage{
+	for value := range secondStage {
 		fmt.Println("In print for value ", value)
+		fmt.Println(value)
+	}
+}
+
+func BufferedChannels() {
+	fmt.Println("Buffered channels ---------------------")
+	nums := []int{2, 6, 3, 7}
+	ch := make(chan int, len(nums))
+
+	// Writing to the channel
+	// go func(){
+	// 	for _, value := range nums{
+	// 		ch <- value
+	// 	}
+	// 	close(ch)
+	// }()
+
+	// Alternative
+	for _, value := range nums {
+		ch <- value
+	}
+	close(ch)
+
+	for value := range ch {
 		fmt.Println(value)
 	}
 }
@@ -92,4 +115,16 @@ func Channels() {
 	// Stage 3: Print all the squared numbers
 	Task2()
 
+	BufferedChannels()
+
 }
+
+/*
+Key notes:
+- Channels are mechanism of communication between goroutines.
+- Range works on closed channels else it waits forever and is a deadlock situation
+- Channel must be closed
+- Buffered channels has fixed size and doesn't block until the buffer is full it pushes without blocks.
+- Unbuffered channels are of size 1 means everything that is written must be read at the same time
+else it blocks the execution flow.
+*/
